0f47380573d1bd459fc0a4571df5ae6237602621
Ravish Kumar Verma <brainstrom.alex@gmail.com>
2026-06-29 20:59:33 +0530

    add condition1

--- main.py
+++ main.py
@@ -24,3 +24,21 @@ choice = ['''
 ''']
 import random
 computer_choice = random.randint(0, 2)
+
+if me == 0:
+    print('''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+''')
+    print("Computer Choice ")
+    print(choice[computer_choice])
+    if computer_choice == 0:
+        print("It's a draw")
+    elif computer_choice == 1:
+        print("You Lose")
+    elif computer_choice == 2:
+        print("You Win")
